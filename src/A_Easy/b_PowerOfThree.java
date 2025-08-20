c4ce30615a0bc02094fdbf6270462e37ff749093
Manish Choudhary <me.developer4u@gmail.com>
2025-08-20 10:28:02 +0530

    Update

---
+++ src/A_Easy/b_PowerOfThree.java
@@ -0,0 +1,12 @@
+package A_Easy;
+
+public class b_PowerOfThree {
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0) return false;
+
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+    }
+}
