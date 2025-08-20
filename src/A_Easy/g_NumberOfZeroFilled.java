fff652e869d0d6671599d7d54ac15af96eff2f04
Manish Choudhary <me.developer4u@gmail.com>
2025-08-20 10:51:37 +0530

    Update

--- src/A_Easy/g_NumberOfZeroFilled.java
+++ src/A_Easy/g_NumberOfZeroFilled.java
@@ -1,5 +1,19 @@
 package A_Easy;
 
 public class g_NumberOfZeroFilled {
+    public long zeroFilledSubarray(int[] nums) {
+        long count = 0;   // total number of zero-filled subarrays
+        long streak = 0;  // current consecutive zero count
+
+        for (int num : nums) {
+            if (num == 0) {
+                streak++;
+                count += streak; // add subarrays ending at this index
+            } else {
+                streak = 0; // reset when non-zero
+            }
+        }
+        return count;
+    }
 
 }
